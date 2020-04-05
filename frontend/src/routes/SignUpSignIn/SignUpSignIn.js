aef0d83e7b3413c7d1bd5fd2d33d88dc07716009
steve990 <steve@stevecodeguy.com>
2020-04-05 11:57:01 -0700

    Changed SignUpSignIn to have buttons selectable

--- frontend/src/routes/SignUpSignIn/SignUpSignIn.js
+++ frontend/src/routes/SignUpSignIn/SignUpSignIn.js
@@ -7,8 +7,8 @@ export default function Login() {
   return (
     <div id='login'>
       <nav>
-        <li><Link to="/newuser">Sign Up</Link></li>
-        <li><Link to="/login">Log In</Link></li>
+        <Link to="/newuser"><li>Sign Up</li></Link>
+        <Link to="/login"><li>Log In</li></Link>
       </nav>
     </div>
   );
